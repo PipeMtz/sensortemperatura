7d4c97870a188d305ab5c8c084f8e7c8b5b65ffe
Felipe Martinez <94323902+PipeMtz@users.noreply.github.com>
2024-05-21 18:49:13 -0500

    Add files via upload

---
+++ sistemas-programables/SensorTemperatura/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace SensorTemperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnMotorOff = new System.Windows.Forms.Button();
+            this.btnMotorOn = new System.Windows.Forms.Button();
+            this.lblTemperatura = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.lblMotor = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtLogs = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnMotorOff);
+            this.groupBox1.Controls.Add(this.btnMotorOn);
+            this.groupBox1.Controls.Add(this.lblTemperatura);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 351);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sensor de Temperatura";
+            // 
+            // btnMotorOff
+            // 
+            this.btnMotorOff.Location = new System.Drawing.Point(148, 254);
+            this.btnMotorOff.Name = "btnMotorOff";
+            this.btnMotorOff.Size = new System.Drawing.Size(75, 23);
+            this.btnMotorOff.TabIndex = 3;
+            this.btnMotorOff.Text = "OFF";
+            this.btnMotorOff.UseVisualStyleBackColor = true;
+            // 
+            // btnMotorOn
+            // 
+            this.btnMotorOn.Location = new System.Drawing.Point(31, 254);
+            this.btnMotorOn.Name = "btnMotorOn";
+            this.btnMotorOn.Size = new System.Drawing.Size(75, 23);
+            this.btnMotorOn.TabIndex = 2;
+            this.btnMotorOn.Text = "ON";
+            this.btnMotorOn.UseVisualStyleBackColor = true;
+            // 
+            // lblTemperatura
+            // 
+            this.lblTemperatura.AutoSize = true;
+            this.lblTemperatura.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemperatura.Location = new System.Drawing.Point(50, 100);
+            this.lblTemperatura.Name = "lblTemperatura";
+            this.lblTemperatura.Size = new System.Drawing.Size(139, 43);
+            this.lblTemperatura.TabIndex = 1;
+            this.lblTemperatura.Text = "grados";
+            this.lblTemperatura.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTemperatura.Click += new System.EventHandler(this.lblTemperatura_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // lblMotor
+            // 
+            this.lblMotor.AutoSize = true;
+            this.lblMotor.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMotor.Location = new System.Drawing.Point(542, 62);
+            this.lblMotor.Name = "lblMotor";
+            this.lblMotor.Size = new System.Drawing.Size(159, 34);
+            this.lblMotor.TabIndex = 1;
+            this.lblMotor.Text = "MOTOR OFF";
+            this.lblMotor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMotor.Click += new System.EventHandler(this.lblMotor_Click);
+            // 
+            // txtLogs
+            // 
+            this.txtLogs.Location = new System.Drawing.Point(1009, 12);
+            this.txtLogs.Name = "txtLogs";
+            this.txtLogs.Size = new System.Drawing.Size(490, 20);
+            this.txtLogs.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(417, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGridView1.Location = new System.Drawing.Point(996, 112);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridView1.Size = new System.Drawing.Size(522, 391);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(43, 547);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(1671, 367);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(417, 112);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Temperatura:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Control manual del motor:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1779, 941);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtLogs);
+            this.Controls.Add(this.lblMotor);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RadiaPLUS - Interfaz para Control de Temperatura";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTemperatura;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label lblMotor;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtLogs;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnMotorOff;
+        private System.Windows.Forms.Button btnMotorOn;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ sistemas-programables/SensorTemperatura/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SensorTemperatura
+{
+    public partial class Form1 : Form
+    {
+        private bool motorEncendido = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Configuración inicial del puerto serie
+                serialPort1.PortName = "COM3";
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+
+                // Configuración del temporizador
+                timer1.Interval = 500; // Reducir el intervalo a 500 milisegundos
+                timer1.Start();
+
+                // Inicializar DataGridView
+                InitializeDataGridView();
+
+                // Inicializar Chart
+                InitializeChart();
+
+                Log("Aplicación iniciada correctamente.");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                Log("Error al iniciar la aplicación: " + error.Message);
+            }
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "Hora";
+            dataGridView1.Columns[1].Name = "Temperatura";
+            dataGridView1.Columns[2].Name = "Estado Motor";
+            dataGridView1.Columns[3].Name = "Peligro";
+        }
+
+        private void InitializeChart()
+        {
+            chart1.Series.Clear();
+            var series = new Series
+            {
+                Name = "Temperature",
+                Color = Color.Blue,
+                ChartType = SeriesChartType.Line
+            };
+            chart1.Series.Add(series);
+            chart1.ChartAreas[0].AxisX.LabelStyle.Enabled = false; // Deshabilitar etiquetas del eje X
+            chart1.ChartAreas[0].AxisY.Title = "Temperature (C)";
+            chart1.ChartAreas[0].AxisX.Title = ""; // No mostrar título en el eje X
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                timer1.Stop();
+                serialPort1.Close();
+                Log("Puerto serie cerrado.");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                Log("Error al cerrar el puerto serie: " + err.Message);
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = serialPort1.ReadLine();
+                Log("Datos recibidos: " + data);
+
+                string[] parts = data.Split(',');
+
+                if (parts.Length == 2)
+                {
+                    UpdateUI(parts[0], parts[1]);
+                }
+                else
+                {
+                    Log("Error: Los datos recibidos no tienen el formato esperado.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("Error en el manejo de datos recibidos: " + ex.Message);
+            }
+        }
+
+        private void UpdateUI(string tempStr, string motorState)
+        {
+            if (lblTemperatura.InvokeRequired || lblMotor.InvokeRequired)
+            {
+                this.Invoke(new Action<string, string>(UpdateUI), new object[] { tempStr, motorState });
+                return;
+            }
+
+            double temp = double.Parse(tempStr);
+            lblTemperatura.Text = tempStr + " C";
+
+            if (temp >= 33)
+            {
+                lblMotor.Text = "ON";
+                lblMotor.BackColor = Color.Green;
+                pictureBox1.Enabled = false;
+                pictureBox2.Enabled = true;
+            }
+            else
+            {
+                lblMotor.Text = "OFF";
+                lblMotor.BackColor = Color.Red;
+                pictureBox1.Enabled = true;
+                pictureBox2.Enabled = false;
+            }
+
+            Color dangerColor;
+            if (temp <= 20)
+            {
+                dangerColor = Color.White;
+            }
+            else if (temp <= 30)
+            {
+                dangerColor = Color.Green;
+            }
+            else if (temp <= 40)
+            {
+                dangerColor = Color.Yellow;
+            }
+            else if (temp <= 50)
+            {
+                dangerColor = Color.Orange;
+            }
+            else
+            {
+                dangerColor = Color.Red;
+            }
+            lblTemperatura.BackColor = dangerColor;
+
+            // Actualizar gráfico
+            UpdateChart(tempStr);
+
+            // Actualizar DataGridView
+            UpdateDataGridView(temp, motorState);
+        }
+
+        private void UpdateChart(string tempStr)
+        {
+            if (chart1.InvokeRequired)
+            {
+                chart1.Invoke(new Action<string>(UpdateChart), new object[] { tempStr });
+                return;
+            }
+
+            double temp;
+            if (double.TryParse(tempStr, out temp))
+            {
+                var now = DateTime.Now;
+                chart1.Series["Temperature"].Points.AddXY(now, temp);
+
+                // Remover puntos viejos para mantener solo los últimos 10 segundos
+                while (chart1.Series["Temperature"].Points.Count > 0 &&
+                       chart1.Series["Temperature"].Points[0].XValue < (now - TimeSpan.FromSeconds(10)).ToOADate())
+                {
+                    chart1.Series["Temperature"].Points.RemoveAt(0);
+                }
+
+                chart1.ChartAreas[0].RecalculateAxesScale();
+            }
+        }
+
+        private void UpdateDataGridView(double temp, string motorState)
+        {
+            string hourMinute = DateTime.Now.ToString("HH:mm tt");
+            string tempStr = $"{temp:F1} C";
+            string motorStatus = motorState == "ON" ? "ON" : "OFF";
+            Color motorColor = motorState == "ON" ? Color.Green : Color.Red;
+
+            string dangerLevel;
+            Color dangerColor;
+            if (temp <= 20)
+            {
+                dangerLevel = "";
+                dangerColor = Color.White;
+            }
+            else if (temp <= 30)
+            {
+                dangerLevel = "Normal";
+                dangerColor = Color.Green;
+            }
+            else if (temp <= 40)
+            {
+                dangerLevel = "Atención";
+                dangerColor = Color.Yellow;
+            }
+            else if (temp <= 50)
+            {
+                dangerLevel = "Precaución";
+                dangerColor = Color.Orange;
+            }
+            else
+            {
+                dangerLevel = "Peligro";
+                dangerColor = Color.Red;
+            }
+
+            // Insertar fila al principio del DataGridView
+            dataGridView1.Rows.Insert(0, hourMinute, tempStr, motorStatus, dangerLevel);
+            dataGridView1.Rows[0].Cells[2].Style.BackColor = motorColor;
+            dataGridView1.Rows[0].Cells[3].Style.BackColor = dangerColor;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Log("Solicitando actualización al Arduino.");
+                serialPort1.WriteLine("GET_DATA");
+            }
+            catch (Exception ex)
+            {
+                Log("Error al solicitar actualización al Arduino: " + ex.Message);
+            }
+        }
+
+        private void Log(string message)
+        {
+            if (txtLogs.InvokeRequired)
+            {
+                txtLogs.Invoke(new Action<string>(Log), new object[] { message });
+            }
+            else
+            {
+                txtLogs.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " - " + message + Environment.NewLine);
+            }
+        }
+
+        private void lblTemperatura_Click(object sender, EventArgs e)
+        {
+            // Método sin uso, se puede eliminar
+        }
+
+        private void lblMotor_Click(object sender, EventArgs e)
+        {
+            // Método sin uso, se puede eliminar
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // Método sin uso, se puede eliminar
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
